bef2ff0aa632f172b35478733a05ff541efb19d3
Jacob Bourne <jbourne2021@fau.edu>
2023-02-08 17:46:37 -0500

    lots of progress on the cli and logging

--- src/git.rs
+++ src/git.rs
@@ -1,57 +1,99 @@
+use git2::{Diff, DiffFormat, DiffOptions, Error, IndexAddOption, Repository};
+use log::debug;
+use serde_json::Value;
 use std::{collections::HashMap, path::PathBuf};
 
-use crate::Value;
-use git2::Repository;
-use reqwest::Client;
-
-#[derive(Debug)]
 pub struct Git {
     local_path: PathBuf,
-    client: Option<reqwest::blocking::Client>,
-    git_remote_token: Option<String>,
-    git_remote_url: Option<String>,
+    github_token: Option<String>,
+    github_url: Option<String>,
+
+    // flags
     auto_add: bool,
-    auto_ai: bool,
+    sign_commit: bool,
+    key_id: String,
+    key_signature: String,
+
+    // stuff to set up
+    client: Option<reqwest::blocking::Client>,
 }
 
 impl Git {
+    // This is if you want to make PRs
     pub fn new(
         local_path: PathBuf,
         client: reqwest::blocking::Client,
-        token: String,
-        url: String,
+        github_token: String,
+        github_url: String,
         auto_add: Option<bool>,
-        auto_ai: Option<bool>,
+        sign_commit: Option<bool>,
+        key_id: Option<String>,
+        key_signature: Option<String>,
     ) -> Self {
         let git = Git {
             local_path: local_path,
             client: Some(client),
-            git_remote_token: Some(token),
-            git_remote_url: Some(url),
+            github_token: Some(github_token),
+            github_url: Some(github_url),
             auto_add: auto_add.unwrap_or(false),
-            auto_ai: auto_ai.unwrap_or(false),
+            sign_commit: sign_commit.unwrap_or(false),
+            key_id: key_id.unwrap_or_default(),
+            key_signature: key_signature.unwrap_or_default(),
         };
         return git;
     }
 
+    // This is for commit only
     pub fn new_local_only(
         local_path: PathBuf,
         auto_add: Option<bool>,
-        auto_ai: Option<bool>,
+        sign_commit: Option<bool>,
+        key_id: Option<String>,
+        key_signature: Option<String>,
     ) -> Self {
         let git = Git {
             local_path: local_path,
             client: None,
-            git_remote_token: None,
-            git_remote_url: None,
+            github_token: None,
+            github_url: None,
             auto_add: auto_add.unwrap_or(false),
-            auto_ai: auto_ai.unwrap_or(false),
+            sign_commit: sign_commit.unwrap_or(false),
+            key_id: key_id.unwrap_or_default(),
+            key_signature: key_signature.unwrap_or_default(),
         };
         return git;
     }
 
-    pub fn open_repo(&self) -> Result<Repository, git2::Error> {
-        let repo = git2::Repository::open(&self.local_path);
-        return repo;
+    fn _add_files_to_index(self, repo: &Repository) -> Result<(), Error> {
+        debug!("Adding untracked files to index");
+        let mut index = repo.index()?;
+        index.add_all(["*"].iter(), IndexAddOption::DEFAULT, None)?;
+        index.write()?;
+        Ok(())
+    }
+
+    pub fn get_diff(self) {
+        debug!("Opening Repo");
+        let repo = git2::Repository::open(&self.local_path).expect("Cannot open repo");
+        // If auto add is set, call the add function
+        debug!("Checking auto-add");
+        debug!("auto_add = {:#?}", &self.auto_add);
+        if self.auto_add {
+            self._add_files_to_index(&repo)
+                .expect("Error Adding New Files To The Index");
+        }
+        let head = &repo.head().expect("Cannot get HEAD");
+        let oid = &head.target().unwrap();
+        let commit = &repo.find_commit(*oid).expect("Cannot Find HEAD commit");
+        let old_tree = &commit.tree().expect("Error getting HEAD tree");
+        let index = &repo.index().expect("Error getting Index");
+        let diff = repo
+            .diff_tree_to_index(
+                Some(&old_tree),
+                Some(&index),
+                Some(&mut DiffOptions::default()),
+            )
+            .expect("Unable to get DIFF");
+        diff.print(DiffFormat::Raw, true);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::error::Error;
-use std::fs::{self};
+use dirs_next::home_dir;
+use log::{debug, info, log_enabled, warn};
+use serde::de::Error;
+use serde_json::{from_reader, value, Value};
+
+use std::collections::HashMap;
+use std::fs::File;
+use std::hash::Hash;
+use std::io::{Read, Write};
+use std::{env, fs};
 
 use std::path::PathBuf;
 
@@ -10,55 +16,67 @@ use clap::{Parser, Subcommand};
 pub mod ai;
 pub mod git;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
 struct Cli {
-    /// set GitHub token
-    #[arg(long = "git_token", value_name = "GIT_TOKEN")]
-    git_remote_token: Option<String>,
+    /// set GitHub API token
+    #[arg(long = "git_api_token", value_name = "API_TOKEN")]
+    github_token: Option<String>,
 
-    /// set Git remote url
-    #[arg(long = "git_url", value_name = "GIT_URL")]
-    git_remote_url: Option<String>,
+    /// set GitHub API url
+    #[arg(long = "git_api_url", value_name = "AI_URL", value_hint = clap::ValueHint::Url)]
+    github_url: Option<String>,
 
     /// set OpenAI token
-    #[arg(long = "ai_token", value_name = "AI_TOKEN")]
+    #[arg(long = "ai_api_token", value_name = "API_TOKEN")]
     open_ai_token: Option<String>,
 
     /// set OpenAI url
-    #[arg(long = "ai_url", value_name = "AI_URL")]
+    #[arg(long = "ai_api_url", value_name = "AI_URL", value_hint = clap::ValueHint::Url)]
     open_ai_url: Option<String>,
 
     /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
+    #[arg(short, long, value_name = "FILE", value_hint = clap::ValueHint::DirPath)]
     config: Option<PathBuf>,
 
     /// Sets a custom local repo, you should probably not use this
-    #[arg(short, long, value_name = "REPO")]
+    #[arg(short, long, value_name = "REPO", value_hint = clap::ValueHint::DirPath)]
     local_repo: Option<PathBuf>,
 
     /// Turn Verbose Mode on
     #[arg(short, long)]
-    verbose: bool,
+    verbose: Option<bool>,
 
     /// Turn Stochastic Mode on
     #[arg(short, long)]
-    stochastic: bool,
+    stochastic: Option<bool>,
 
     /// Turns Auto Add mode on which adds . to git before making the commit DANGEROUS
     #[arg(short, long)]
-    auto_add: bool,
+    auto_add: Option<bool>,
 
     /// Turns Auto AI mode on automatically accepts the AI message without review DANGEROUS
     #[arg(short = 'i', long = "ai")]
-    auto_ai: bool,
+    auto_ai: Option<bool>,
+
+    /// Number of times to try the AI: Note OpenAI Chatbot is not Idenpotent
+    #[arg(short, long, value_name = "TRIES", value_parser=_allowed_num_tries)]
+    num_tries: Option<u64>,
+
+    /// Sign Commits, if set some variables must be added to settings.json
+    #[arg(short, long)]
+    sign_commit: Option<bool>,
+
+    /// Signing Key ID: Note, ignored if sign_commit=false
+    #[arg(short, long)]
+    signature_id: Option<String>,
 
     #[command(subcommand)]
     command: Option<Commands>,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum Commands {
     /// Generate Commit Message
     Commit {},
@@ -73,107 +91,169 @@ enum Commands {
     Models {},
 }
 
-/// JSON Structs
-#[derive(Serialize, Deserialize, Debug)]
-struct Settings {
-    pub ai_information: AiInformation,
-    pub git_information: Option<GitInformation>,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct AiInformation {
-    pub api_url: String,
-    pub api_token: String,
+fn get_settings(p: Option<PathBuf>) -> Result<Value, Box<dyn std::error::Error>> {
+    let default_path: PathBuf = [
+        home_dir()
+            .expect("HOMEDIR Not Set")
+            .to_str()
+            .expect("Invalid HOMEDIR"),
+        ".gitai",
+        "settings.json",
+    ]
+    .iter()
+    .collect();
+    let settings_path = p.unwrap_or(default_path);
+    debug!("Checking {:#?} to see if it exists", &settings_path);
+    if settings_path.exists() {
+        let mut file = File::open(settings_path.as_path()).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)
+            .expect("couldn't read the file");
+        let data: serde_json::Value =
+            from_reader(contents.as_bytes()).expect("couldn't parse the JSON");
+        return Ok(data);
+    } else {
+        debug!("{:#?} does not exist, creating a blank one", &settings_path);
+        fs::create_dir_all(&settings_path)?;
+        let mut f = File::create(&settings_path).unwrap();
+        f.write_all(DEFAULT_FILE.as_bytes())
+            .expect("Unable to write default file");
+        return Err("No settings.json exists, we created a blank one at ~/.gitai".into());
+    }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct GitInformation {
-    pub remote_token: Option<String>,
-    pub remote_url: Option<String>,
-}
-
-fn load_json_file(path: PathBuf) -> Result<Settings, Box<dyn Error>> {
-    // Open the file in read-only mode with buffer.
-    let settings_file_as_string =
-        fs::read_to_string(path.as_os_str()).expect("Unable to read file");
-    let settings = serde_json::from_str(&settings_file_as_string).expect("JSON not parsing");
-    Ok(settings)
+fn _allowed_num_tries(s: &str) -> Result<u8, String> {
+    clap_num::number_range(s, 1, 3)
 }
 
 fn main() {
+    env_logger::init();
+    info!("Initializing GitAI");
+
+    debug!("Parsing CLI");
     let cli = Cli::parse();
+    debug!("{:#?}", cli);
 
-    let config_file: PathBuf = match cli.config {
-        None => {
-            let home_dir = dirs_next::home_dir().expect("User Home Dir Not Set");
-            let mut path = PathBuf::new();
-            path.push(home_dir.as_os_str());
-            path.push(".gitai");
-            path.push("settings.json");
-            path
-        }
-        Some(c) => c,
-    };
+    debug!("Reading settings file");
+    let settings = get_settings(cli.config).unwrap();
 
-    let settings = load_json_file(config_file).expect("Problem reading settings.json");
+    debug!("Setting Variables");
+    let ai_token = cli
+        .open_ai_token
+        .or(env::var("AI_OPENAI_TOKEN").ok())
+        .or(Some(settings["ai_information"]["api_token"].to_string()))
+        .expect("AI_TOKEN Must be set");
 
-    let open_ai_token: String = if let Some(a) = cli.open_ai_token {
-        a
-    } else {
-        settings.ai_information.api_token
-    };
+    let ai_url = cli
+        .open_ai_url
+        .or(env::var("AI_OPENAI_URL").ok())
+        .or(Some(settings["ai_information"]["api_url"].to_string()))
+        .expect("AI_URL Must be set");
 
-    let open_ai_url: String = if let Some(a) = cli.open_ai_url {
-        a
-    } else {
-        settings.ai_information.api_url
-    };
+    let git_token = cli
+        .github_token
+        .or(env::var("AI_OPENAI_URL").ok())
+        .or(Some(settings["git_information"]["api_token"].to_string()));
 
-    let git_remote_token: Option<String> = if let Some(g) = cli.git_remote_token {
-        Some(g)
-    } else {
-        if let Some(g) = &settings.git_information {
-            g.remote_token.to_owned()
-        } else {
-            None
-        }
-    };
-
-    let git_remote_url: Option<String> = if let Some(i) = cli.git_remote_url {
-        Some(i)
-    } else {
-        if let Some(g) = &settings.git_information {
-            g.remote_url.to_owned()
-        } else {
-            None
-        }
-    };
-
-    let git_local_path = if let Some(i) = cli.local_repo {
-        i
-    } else {
-        PathBuf::from(".")
-    };
+    let git_url = cli
+        .github_url
+        .or(env::var("AI_OPENAI_URL").ok())
+        .or(Some(settings["git_information"]["api_url"].to_string()));
 
     // Flags
-    let verbose: bool = cli.verbose;
-    let stochastic: bool = cli.stochastic;
-    let auto_add: bool = cli.auto_add;
-    let auto_ai: bool = cli.auto_ai;
+    let auto_ai = cli
+        .auto_ai
+        .or(settings["ai_information"]["options"]["auto_ai"].as_bool())
+        .unwrap_or(false);
 
+    let auto_add = cli
+        .auto_add
+        .or(settings["git_information"]["options"]["auto_add"].as_bool())
+        .unwrap_or(false);
+
+    let stochastic = cli
+        .stochastic
+        .or(settings["ai_information"]["options"]["stochastic"].as_bool())
+        .unwrap_or(false);
+
+    let num_tries = cli
+        .num_tries
+        .or(settings["ai_information"]["options"]["num_tries"].as_u64())
+        .unwrap_or(1);
+
+    let sign_commit = cli
+        .sign_commit
+        .or(settings["git_information"]["options"]["sign_commit"].as_bool())
+        .unwrap_or(false);
+
+    let mut key_id = String::new();
+    let mut key_signature = String::new();
+    if sign_commit {
+        let key_id = cli
+            .signature_id
+            .or(Some(
+                settings["git_information"]["options"]["gpg_key_id"].to_string(),
+            ))
+            .expect("If signing, the key ID musy be set");
+        let key_signature =
+            Some(settings["git_information"]["options"]["gpg_key_signature"].to_string());
+    }
+
+    let local_repo = cli.local_repo.unwrap_or(PathBuf::from("."));
+
+    debug!("Variables Set OpenAI Url={:#?} should not be null", ai_url);
+    debug!(
+        "Local Repo={:#?} this should probably be '.' unless you have good reason",
+        local_repo
+    );
+
+    debug!("Matching CLI Command");
     match &cli.command {
         Some(Commands::Commit {}) => {
-            let git = git::Git::new_local_only(git_local_path, Some(auto_add), Some(auto_ai));
-            let repo = git.open_repo().unwrap();
-            let origin = repo.find_remote("origin").unwrap();
+            let git = git::Git::new_local_only(
+                local_repo,
+                Some(auto_add),
+                Some(sign_commit),
+                Some(key_id),
+                Some(key_signature),
+            );
         }
         Some(Commands::PR { from, to }) => {
-            println!("pr {from} -> {to}");
+            println!("PR From {} to {}", from, to);
+            let git = git::Git::new(
+                local_repo,
+                reqwest::blocking::Client::new(),
+                git_token.unwrap(),
+                git_url.unwrap(),
+                Some(auto_add),
+                Some(sign_commit),
+                Some(key_id),
+                Some(key_signature),
+            );
+            print!("From {:#?} To {:#?}", from, to);
         }
         Some(Commands::Models {}) => {
-            let ai = ai::AI::new(&open_ai_url, &open_ai_token);
-            let models = ai.get_models();
-            print!("{:#?}", models);
+            println!("Get Available Models");
         }
-        None => {}
+        None => (),
     }
 }
+
+const DEFAULT_FILE: &str = "
+{
+    \"ai_information\": {
+      \"api_url\": \"<OPEN_API_URL>\",
+      \"api_token\": \"<OPEN_AI_TOKEN>\",
+      \"options\": {
+        \"stochastic\": true
+      }
+    },
+    \"git_information\": {
+       \"remote_url\": \"<GITHUB_URL\",
+      \"remote_token\": \"<GITHUB_TOKEN>\",
+      \"options\": {
+        \"sign_commits\": true
+      }
+    }
+  }
+";
